5916d3ebea790c82bee3df778a8b34a10a4e51dc
Sujit M <s.mahavarkar@gmail.com>
2024-04-12 03:17:02 +0530

    Updates the config for Db

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,23 @@
 let app_base_domain = "http://localhost:3030";
 
-let dev = {
+let db = {
+	DATABASE_HOST: process.env.DATABASE_HOST || "localhost", // or the socket path
+	DATABASE_NAME: process.env.DATABASE_NAME || "test",
+	DATABASE_USERNAME: process.env.DATABASE_USERNAME || "test_user",
+	DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "idontknow",
+	DATABASE_PORT: process.env.DATABASE_PORT || 5432,
+	DATABASE_DIALECT: process.env.DATABASE_DIALECT || "postgres",
+	NODE_ENV: process.env.NODE_ENV || "development",
+	SCHEMA: "public",
+};
+
+let config = {
 	app: {
 		base_domain: app_base_domain,
 		port: process.env.PORT || "3030",
 		name: "Boilerplate API Platform",
 	},
+	db: db,
 };
-
-let config = dev;
 
 module.exports = config;
