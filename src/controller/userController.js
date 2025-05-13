fcdcfc25393b0c5ae6a74191ac696701670377f2
Sujit M <s.mahavarkar@gmail.com>
2025-05-14 03:17:25 +0530

    Updates the user/guest creation and respond with cookie

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,15 +3,21 @@ const createUserService = require("../services/userServices");
 const createUser = async (req, res) => {
 	try {
 		const { email = null, username, isGuest = true } = req.body;
+		if (req.cookies.user) {
+			return res
+				.status(400)
+				.json({ success: false, message: "User already exists" });
+		}
 		const user = await createUserService.createUser({
 			email,
 			username,
 			isGuest,
 		});
-		console.log({ cookies: req.cookies });
+
 		res.cookie("user", JSON.stringify(user), {
 			maxAge: 3600000,
 			httpOnly: true,
+			sameSite: "Lax",
 			secure: false,
 		});
 		res.status(201).json({ success: true, user });
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,9 +1,11 @@
-
-
-
 module.exports = {
-	auth: async()=>{
-		console.log('i am in auth middleware')
-	}
-
-}
+	auth: async (req, res, next) => {
+		//check if the user is guest or not
+		if (!req.cookies.user) {
+			return res.status(401).json({ error: "Unauthorized" });
+		}
+		const user = JSON.parse(req.cookies.user);
+		req.user = user;
+		next();
+	},
+};
